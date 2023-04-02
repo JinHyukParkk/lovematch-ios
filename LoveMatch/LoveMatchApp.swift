65464d7b3fec26ff3633208afab16a88cddda48c
hyuk <hyuk@woowahan.com>
2023-04-02 23:19:48 +0900

    Initial Commit

---
+++ LoveMatch/LoveMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveMatchApp.swift
+//  LoveMatch
+//
+//  Created by hyuk on 2023/04/02.
+//
+
+import SwiftUI
+
+@main
+struct LoveMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
